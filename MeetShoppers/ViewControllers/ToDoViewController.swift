59e34e87f3fa73488e38d46b368ec1782f1bee99
Keloc13 <kvnyn10@gmail.com>
2018-04-16 20:23:36 -0700

    rid of error

--- MeetShoppers/ViewControllers/ToDoViewController.swift
+++ MeetShoppers/ViewControllers/ToDoViewController.swift
@@ -8,14 +8,8 @@
 
 import UIKit
 
-class ToDoViewController: UIViewController,UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
+class ToDoViewController: UIViewController{
+ 
     
 
     override func viewDidLoad() {
