4cfd613b4b5451f83a9e5fea3efe1d8b1eed86ff
Keloc13 <kvnyn10@gmail.com>
2018-04-24 21:45:37 -0700

    Adding information

--- MeetShoppers/Controller/StoreDetailViewController.swift
+++ MeetShoppers/Controller/StoreDetailViewController.swift
@@ -79,15 +79,4 @@ class StoreDetailViewController: UIViewController, UITableViewDataSource, UITabl
         }
         return 1
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
