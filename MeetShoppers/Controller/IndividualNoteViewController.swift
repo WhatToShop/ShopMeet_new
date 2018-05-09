c7f1a55ebedd09c7dcca2698a680f46e9af46c98
Keloc13 <kvnyn10@gmail.com>
2018-05-09 13:00:03 -0700

    temp Commit

---
+++ MeetShoppers/Controller/IndividualNoteViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IndividualNoteViewController.swift
+//  MeetShoppers
+//
+//  Created by Kevin Nguyen on 5/7/18.
+//  Copyright © 2018 KevinVuNguyen. All rights reserved.
+//
+
+import UIKit
+
+class IndividualNoteViewController: UIViewController {
+
+    var value: note?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
